8b83bfc7d59211c33c0e538e00cfa446aa82e963
sumanbro0 <pandey.suman258@gmail.com>
2023-11-21 20:12:16 +0545

    task 2 complete

--- day-1/guessing_game.py
+++ day-1/guessing_game.py
@@ -1,7 +1,8 @@
 import random
-guessing_number=random.randint(1,100)
 print("Welcome to the Number guessing game")
+guessing_number=random.randint(1,100)
 attempt=3
+
 while(attempt>0):
     guessed_number=int(input("Enter Your Guess: "))
     if guessed_number==guessing_number:
@@ -9,12 +10,8 @@ while(attempt>0):
         break
     elif guessed_number>guessing_number:
         print("Your guess is higher than the guessing number")
-        guessed_number=int(input("Enter Your Guess: "))
     elif guessed_number<guessing_number:
         print("Your guess is lower than the guessing number")
-        guessed_number=int(input("Enter Your Guess: "))
     else:
         print("Invalid input")
-
     attempt=attempt-1
-    print(f'{attempt} chances remaining')
--- day-1/odd_even.py
+++ day-1/odd_even.py
@@ -1,5 +1,4 @@
 number=int(input("Enter any number: "))
-
 for num in range(number+1):
     if num%2==0:
         print(num)
