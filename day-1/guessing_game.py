187340948030b16a4882f8bc7bca9ee798f43d16
sumanbro0 <pandey.suman258@gmail.com>
2023-11-21 19:58:47 +0545

    completed task 2

--- day-1/guessing_game.py
+++ day-1/guessing_game.py
@@ -1,3 +1,20 @@
 import random
 guessing_number=random.randint(1,100)
-print(guessing_number)
+print("Welcome to the Number guessing game")
+attempt=3
+while(attempt>0):
+    guessed_number=int(input("Enter Your Guess: "))
+    if guessed_number==guessing_number:
+        print("Congratulations You got it right")
+        break
+    elif guessed_number>guessing_number:
+        print("Your guess is higher than the guessing number")
+        guessed_number=int(input("Enter Your Guess: "))
+    elif guessed_number<guessing_number:
+        print("Your guess is lower than the guessing number")
+        guessed_number=int(input("Enter Your Guess: "))
+    else:
+        print("Invalid input")
+
+    attempt=attempt-1
+    print(f'{attempt} chances remaining')
