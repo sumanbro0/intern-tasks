ffdcd92e8531e29ac142207aec19f659ed03f627
sumanbro0 <pandey.suman258@gmail.com>
2023-11-21 19:47:30 +0545

    first commit

---
+++ day-1/guessing_game.py
@@ -0,0 +1,3 @@
+import random
+guessing_number=random.randint(1,100)
+print(guessing_number)
---
+++ day-1/odd_even.py
@@ -0,0 +1,6 @@
+number=int(input("Enter any number: "))
+
+for num in range(number+1):
+    if num%2==0:
+        print(num)
+    
